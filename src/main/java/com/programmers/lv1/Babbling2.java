1e287723c22ea4464187bccfb5a3d39d5149a3cb
SeHoon <sehoon1989@gmail.com>
2024-09-04 00:01:50 +0900

    feature/cleanup-source

--- src/main/java/com/programmers/lv1/Babbling2.java
+++ src/main/java/com/programmers/lv1/Babbling2.java
@@ -6,32 +6,66 @@ public class Babbling2 {
     /**
      * 옹알이2(https://school.programmers.co.kr/learn/courses/30/lessons/133499)
      */
-    public String solution(String[] babbling) {
+    // public int solution(String[] babbling) {
+    //     int answer = 0;
+    //     String[] speaks = { "aya", "ye", "woo", "ma" };
+    //     List<String> speakList = Arrays.asList(speaks);
+        
+    //     for(String babble : babbling) {
+    //         StringBuilder sb = new StringBuilder();
+    //         String repeatStr = "";
+    //         String appendStr = "";
+    //         String str = babble;
+    //         for(int i=0; i<babble.length(); i++){
+    //             char charAtIndex = babble.charAt(i);
+    //             sb.append(String.valueOf(charAtIndex));
+    //             appendStr = sb.toString();
+    //             if(speakList.contains(appendStr) && !repeatStr.equals(appendStr)){
+    //                 repeatStr = appendStr;
+    //                 str = str.replaceFirst(appendStr, "");
+    //                 sb.setLength(0);
+    //             }
+    //         }
+    //         if(str.equals("")){
+    //             answer++;
+    //         }
+            
+    //     }
+        
+    //     return answer;
+    // }
+
+    // 소스정리
+    public int solution(String[] babbling) {
         int answer = 0;
         String[] speaks = { "aya", "ye", "woo", "ma" };
         List<String> speakList = Arrays.asList(speaks);
-        
-        for(String babble : babbling) {
-            StringBuilder sb = new StringBuilder();
-            String repeatStr = "";
-            String appendStr = "";
-            String str = babble;
-            for(int i=0; i<babble.length(); i++){
-                char charAtIndex = babble.charAt(i);
-                sb.append(String.valueOf(charAtIndex));
-                appendStr = sb.toString();
-                if(speakList.contains(appendStr) && !repeatStr.equals(appendStr)){
-                    repeatStr = appendStr;
-                    str = str.replaceFirst(appendStr, "");
-                    sb.setLength(0);
+    
+        for (String babble : babbling) {
+            String previousWord = "";
+            boolean isValid = true;
+    
+            while (!babble.isEmpty()) {
+                boolean found = false;
+                for (String speak : speakList) {
+                    if (babble.startsWith(speak) && !speak.equals(previousWord)) {
+                        previousWord = speak;
+                        babble = babble.substring(speak.length());
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    isValid = false;
+                    break;
                 }
             }
-            if(str.equals("")){
+    
+            if (isValid) {
                 answer++;
             }
-            
         }
-        
+    
         return answer;
     }
     
